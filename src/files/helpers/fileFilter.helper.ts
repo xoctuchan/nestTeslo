90f6d3c4fd45d9ea4f1efc14efe54437a8880a80
Juan Pablo Xoc Tuchán <xoctuchan@yahoo.es>
2022-12-07 12:32:49 -0600

    Se completa la sección de carga de archivos

---
+++ src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,13 @@
+import { Request } from "express";
+
+export const fileFilter = (req: Request, file: Express.Multer.File, callback: Function) =>{
+
+    if(!file) return callback(new Error('File is empty'), false)
+
+    const fileExtension = file.mimetype.split('/')[1];
+    const validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+    if(validExtensions.includes(fileExtension)){
+        return callback(null, true)
+    }
+    callback(null, false);
+}
