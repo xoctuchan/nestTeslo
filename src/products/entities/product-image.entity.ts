0574bf3bf17979d3905972616f62168fa1b83a03
Juan Pablo Xoc Tuchán <xoctuchan@yahoo.es>
2022-12-07 10:46:32 -0600

    Se finaliza sección 11 - Relaciones entre entidades

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Product } from "./product.entity";
 
-@Entity()
+@Entity({name: 'product_images'})
 export class ProductImage{
     @PrimaryGeneratedColumn()
     id: number;
@@ -11,7 +11,8 @@ export class ProductImage{
 
     @ManyToOne(
         ()=>Product,
-        ( product ) =>product.images
+        ( product ) =>product.images,
+        { onDelete: 'CASCADE'}
     )
     product: Product
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -92,11 +92,23 @@ export class ProductsService {
     
     //Create query runner
     const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     
     try {
-      await this.productRepository.save(product)  
-      return product;
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, { product:{id}});
+        product.images = images.map(image =>this.productImageRepository.create({url: image}))
+      }
+      await queryRunner.manager.save(product);
+      //await this.productRepository.save(product)  
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+      return this.findOnePlain(id);
     } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
       this.handleDBExceptions(error);
     }
   }
@@ -112,6 +124,16 @@ export class ProductsService {
       if(error.code==='22P02')
       throw new BadRequestException(error.routine);
 
+      //console.log(error);
       throw new InternalServerErrorException ("Help!");
+  }
+
+  async deleteAllProducts(){
+    const query = this.productRepository.createQueryBuilder('product');
+    try {
+      return await query.delete().where({}).execute();
+    } catch (error) {
+      this.handleDBExceptions(error)
+    }
   }
 }
