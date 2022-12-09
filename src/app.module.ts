3076520f2d9180fdfd4b1edfd6b848b9c12b7fa7
Juan Pablo Xoc Tuchán <xoctuchan@yahoo.es>
2022-12-09 10:43:13 -0600

    Se completa la sección de JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
@@ -29,7 +30,8 @@ import { AuthModule } from './auth/auth.module';
     CommonModule,
     SeedModule,
     FilesModule,
-    AuthModule
+    AuthModule,
+    MessagesWsModule
   ],
   controllers: [],
   providers: [],
